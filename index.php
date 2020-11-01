d9c5b32f4ca29a14901f6a73b783083e934923c4
hamadialvin <hamadialvin@yahoo.com>
2020-11-01 10:37:58 +0300

    Backend: Login

--- index.php
+++ index.php
@@ -40,17 +40,47 @@ include("config.php");
 
 		                    <div class="mb-4">
 		                        
-								<img src="img/healthitlogo.png">
+								<img src="img/icons/healthit.png">
 											
 		                    </div>
 		                    <h3 class="mb-4 text">Immunization Defaulter Tracker</h3>
-		                    <div class="input-group mb-3">
-		                        <input type="email" class="form-control" placeholder="Email" required>
-		                    </div>
-		                    <div class="input-group mb-4">
-		                        <input type="password" class="form-control" placeholder="password" required>
-		                    </div>
-		                    <button class="btn btn-primary shadow-2 mb-4">Login</button>
+		                    <div class="card-body">
+								<form action="" method="POST">
+				                    <div class="input-group mb-3">
+				                        <input type="email" class="form-control" placeholder="Email" required name="loginEmail">
+				                    </div>
+				                    <div class="input-group mb-4">
+				                        <input type="password" class="form-control" placeholder="password" required name="loginPassword">
+				                    </div>
+				                    <button class="btn btn-primary shadow-2 mb-4" name="buttonLogin">Login</button>
+				                    <br>
+				                    <br>
+
+				                    <?php
+				                    if(isset($_POST['buttonLogin'])){
+
+				                    		  $email = $_POST['loginEmail'];
+											  $password = md5($_POST['loginPassword']);
+											  
+
+
+											  $results = mysqli_query($mysqli, "SELECT * FROM users WHERE email='$email' AND password='$password'");
+											  
+
+											  if (mysqli_num_rows($results) == 1) {
+											  	  echo "You are now logged in as $email";
+											  	  header('location: dashboard.php');
+											  	  
+											  	}else {
+											  	  echo "Wrong username/password combination";
+											  	}		  
+
+				                    }
+
+
+				                    ?>
+				                </form>
+				            </div>
 		                    
                 	</div>
 				</div>
